79b54609d249d8d029a6c6923f8b44863cf3be1e
theanuragharsh <nrgharsh@gmail.com>
2023-02-21 16:54:13 +0530

    Initial commit | Pushing code to github

---
+++ src/main/java/com/twilio/TwilioWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioWebfluxApplication.class, args);
+	}
+
+}
